d1fb59eea061aa7a32885819eba3a49c99db185a
devendr4 <angel.emo.altuve@gmail.com>
2023-08-30 00:10:27 -0400

    pending tasks filter

--- src/app/tasks/pages/new-task/new-task.component.ts
+++ src/app/tasks/pages/new-task/new-task.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
-  styleUrls: ['./new-task.component.scss']
+  styleUrls: ['./new-task.component.scss'],
 })
 export class NewTaskComponent {
+  constructor(private router: Router) {}
 
+  redirect(event: unknown) {
+    console.log('r', event);
+    this.router.navigate(['/']);
+  }
 }
--- src/app/tasks/task-form/task-form.component.ts
+++ src/app/tasks/task-form/task-form.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../models';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
-  styleUrls: ['./task-form.component.scss']
+  styleUrls: ['./task-form.component.scss'],
 })
 export class TaskFormComponent {
-
+  //for editing tasks
+  @Input() task?: Task;
 }
