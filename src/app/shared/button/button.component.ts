33d4da3d5262672edf13cef72c0f85edd7f86a19
devendr4 <angel.emo.altuve@gmail.com>
2023-08-30 12:35:35 -0400

    confirm delete modal

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,13 +1,19 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
   @Output() clicked: EventEmitter<MouseEvent> = new EventEmitter();
   @Input() disabled: boolean;
+  @Input() type?: 'danger' | 'success' | 'warning' | '' = '';
+  @Input() class?: string;
+
+  ngOnInit(): void {
+    console.log(this.type);
+  }
 
   onClick(event: MouseEvent) {
     this.clicked.emit(event);
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,16 @@ import { CardComponent } from './card/card.component';
 import { ButtonComponent } from './button/button.component';
 import { IconBtnComponent } from './icon-btn/icon-btn.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
-  declarations: [CardComponent, ButtonComponent, IconBtnComponent],
+  declarations: [
+    CardComponent,
+    ButtonComponent,
+    IconBtnComponent,
+    ModalComponent,
+  ],
   imports: [CommonModule, AngularSvgIconModule],
-  exports: [CardComponent, ButtonComponent, IconBtnComponent],
+  exports: [CardComponent, ButtonComponent, IconBtnComponent, ModalComponent],
 })
 export class SharedModule {}
