1c8112cb395c5a6a5a982df84a2b9e9aa7d8119b
devendr4 <angel.emo.altuve@gmail.com>
2023-08-29 23:26:44 -0400

    new task form

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @Output() clicked: EventEmitter<MouseEvent> = new EventEmitter();
+
+  onClick(event: MouseEvent) {
+    this.clicked.emit(event);
+  }
+}
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -5,9 +5,12 @@ import { SharedModule } from '../shared/shared.module';
 import { TaskCheckboxComponent } from './task-checkbox/task-checkbox.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { IndexComponent } from './pages/index/index.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 
 @NgModule({
-  declarations: [TaskListComponent, TaskCheckboxComponent, TaskItemComponent],
+  declarations: [TaskListComponent, TaskCheckboxComponent, TaskItemComponent, TaskFormComponent, IndexComponent, NewTaskComponent],
   imports: [CommonModule, SharedModule, AngularSvgIconModule],
   exports: [TaskListComponent],
 })
